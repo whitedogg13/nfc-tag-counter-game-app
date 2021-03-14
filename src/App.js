811813588e72060bbb169a62895c8547a5290fe6
Richie <whitedogg13@gmail.com>
2021-03-14 17:02:12 +0800

    hasNfc-state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
 function App(props) {
+  const [hasNfc, setHasNfc] = React.useState(null);
+
+  if (hasNfc === null) {
+    return null;
+  } else if (!hasNfc) {
+    return (
+      <View style={styles.wrapper}>
+        <Text>You device doesn't support NFC</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.wrapper}>
       <Text>Hello NFC!</Text>
