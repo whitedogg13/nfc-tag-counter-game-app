67715fc7e9eb1ce5b64b14c61f7047e298a74322
Richie <whitedogg13@gmail.com>
2021-03-15 19:59:21 +0800

    draft

--- src/Game.js
+++ src/Game.js
@@ -1,49 +1,12 @@
 import React from 'react';
-import {TouchableOpacity, View, Text, StyleSheet, Alert} from 'react-native';
-import NfcManager, {NfcEvents} from 'react-native-nfc-manager';
+import {TouchableOpacity, View, Text, StyleSheet} from 'react-native';
+import NfcManager from 'react-native-nfc-manager';
 
 function Game(props) {
-  const [startTime, setStartTime] = React.useState(null);
-  const [stopTime, setStopTime] = React.useState(null);
-
-  React.useEffect(() => {
-    let count = 5;
-    NfcManager.setEventListener(NfcEvents.DiscoverTag, (tag) => {
-      count--;
-
-      if (count === 0) {
-        setStopTime(new Date());
-        NfcManager.unregisterTagEvent().catch(() => 0);
-      }
-    });
-
-    NfcManager.setEventListener(NfcEvents.SessionClosed, () => {
-      if (count !== 0) {
-        Alert.alert('Warning', 'Game aborted', [{text: 'OK'}]);
-      }
-    });
-
-    return () => {
-      NfcManager.setEventListener(NfcEvents.DiscoverTag, null);
-      NfcManager.setEventListener(NfcEvents.SessionClosed, null);
-    };
-  }, [startTime]);
-
-  async function startGame() {
-    await NfcManager.registerTagEvent();
-    setStartTime(new Date());
-    setStopTime(null);
-  }
-
   return (
     <View style={styles.wrapper}>
       <Text>NFC Game</Text>
-
-      {startTime && stopTime && (
-        <Text>{`${stopTime.getTime() - startTime.getTime()}`} ms</Text>
-      )}
-
-      <TouchableOpacity style={styles.btn} onPress={startGame}>
+      <TouchableOpacity style={styles.btn}>
         <Text>START</Text>
       </TouchableOpacity>
     </View>
