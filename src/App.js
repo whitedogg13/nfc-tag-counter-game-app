628b7cf78fd85426a0c48a16492966d3ab6b16cc
Richie <whitedogg13@gmail.com>
2021-03-24 07:18:05 +0800

    feat: check if nfc is enabled

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Game from './Game';
 
 function App(props) {
   const [hasNfc, setHasNfc] = React.useState(null);
+  const [enabled, setEnabled] = React.useState(null);
 
   React.useEffect(() => {
     async function checkNfc() {
       const supported = await NfcManager.isSupported();
       if (supported) {
         await NfcManager.start();
+        setEnabled(await NfcManager.isEnabled());
       }
       setHasNfc(supported);
     }
@@ -24,6 +26,26 @@ function App(props) {
     return (
       <View style={styles.wrapper}>
         <Text>You device doesn't support NFC</Text>
+      </View>
+    );
+  } else if (!enabled) {
+    return (
+      <View style={styles.wrapper}>
+        <Text>Your NFC is not enabled!</Text>
+
+        <TouchableOpacity
+          onPress={() => {
+            NfcManager.goToNfcSetting();
+          }}>
+          <Text>GO TO NFC SETTINGS</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={async () => {
+            setEnabled(await NfcManager.isEnabled());
+          }}>
+          <Text>CHECK AGAIN</Text>
+        </TouchableOpacity>
       </View>
     );
   }
